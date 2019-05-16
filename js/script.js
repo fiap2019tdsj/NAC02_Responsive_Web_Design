5476f9434aab85daf0d3eb23ff6ab316eb376257
Renan Silva <renan.gomes.ti@gmail.com.br>
2019-05-16 19:48:47 -0300

    alterar logo

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-// $(function(){
+ $(function(){
 
-//     $("alinha-conteudo div").css("display", "none");
-//     $("alinha-conteudo div#capitulo1").addClass("active");
+     $("alinha-resumo div").css("display", "none");
+     $("alinha-resumo div#capitulo1").addClass("active");
 
-//     $("a").on("click", function( e ){         
-//         e.preventDefault();
+     $("a").on("click", function( e ){         
+         e.preventDefault();
         
-//     	$(".alinha-conteudo div").removeClass("active");
-//         var id = $(this).attr("href"); 
-//         $("#"+id+"").addClass("active");        
-//     });
-// });
+     	$(".alinha-resumo div").removeClass("active");
+         var id = $(this).attr("href"); 
+         $("#"+id+"").addClass("active");        
+     });
+ });
 
-// console.log(capitulo1);
+//console.log(capitulo1);
